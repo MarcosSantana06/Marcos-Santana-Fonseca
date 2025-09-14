8c154ba07f94281c8c842a525850415ee4981db6
MarcosSantana06 <santanafonsecamarcos@gmail.com>
2025-09-13 22:54:28 -0700

    Subiendo mi proyecto a github completo

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+//Este es mi programa de hola mundo para I.P:
+public class HolaMundo{
+public static void main(String[] args) {
+    
+    System.out.println("Hola mundo");
+    System.out.println("Hello world");
+}
+}
